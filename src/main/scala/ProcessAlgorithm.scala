112ee941786c0b4a2b6160c4dca27d14b02345e5
David Suarez <david.suarez@intelygenz.com>
2016-02-09 13:33:51 +0100

    Process Events Sequentially

--- src/main/scala/ProcessAlgorithm.scala
+++ src/main/scala/ProcessAlgorithm.scala
@@ -18,20 +18,35 @@ object ProcessAlgorithm {
 
     // We receive the list of events, in this case just a set of number
     // to sum and substract
-    val listEvent = List(("sumar",-10),("restar", 2), ("restar", -3), ("sumar", 4))
-    val newBalance = listEvent.foldLeft(person)((a, b) => changeStatus(a, b)).balance
-    println("This is the final balance: " + newBalance)
+    val listEvent = List(("sumar",10),("restar", 20), ("restar", 3), ("sumar", 4))
+
+    changeStatusRecursive(listEvent, person).foreach(println)
+
     sc.stop()
 
   }
 
-  def changeStatus(personStatus: Person, eventValue: (String, Int)): Person = {
-    var balance = personStatus.balance
+  def changeStatusRecursive(eventList: List[(String, Int)], person: Person): List[(String, Int)] = {
+
+    val accum = (Person(0.0),List(("start", 0)))
+    val newBalance = eventList.foldLeft(accum)((a, b) => (changeStatus(a, b)))
+
+    newBalance._2
+  }
+
+  def changeStatus(acc: (Person, List[(String, Int)]), eventValue: (String, Int)): (Person, List[(String, Int)]) = {
+    val person = acc._1
+    var eventList = acc._2
+
+    var balance = person.balance
     if(eventValue._1 == "sumar"){
       balance += eventValue._2
-    } else {
+      eventList = eventList :+ eventValue
+    } else if (eventValue._1 == "restar" && person.balance > eventValue._2){
       balance -= eventValue._2
+      eventList = eventList :+ eventValue
     }
-    Person(balance)
+    (Person(balance), eventList)
   }
+
 }
